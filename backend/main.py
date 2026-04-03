0f650a88214fbe2f5444713145117dead53be0cc
JohnLizama <john3lizama@gmail.com>
2026-04-03 09:54:18 -0400

    skeleton for user/playlist endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 import fastapi
+from backend.routes import users, playlists
 
 app = fastapi.FastAPI()
+app.include_router(users.router)
+app.include_router(playlists.router)
 
 @app.get("/")
 async def root():

9a1c406b27ff2c4a7a9bfbab2892cb04061de9b4
JohnLizama <john3lizama@gmail.com>
2026-04-22 22:13:43 -0400

    feat: include albums and songs routers in the main application

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 import fastapi
-from backend.routes import users, playlists
+from backend.routes import albums, songs, users, playlists
+from backend.db.database import Base, engine
+
+# Create the database tables from the models
+Base.metadata.create_all(bind=engine)
 
 app = fastapi.FastAPI()
 app.include_router(users.router)
 app.include_router(playlists.router)
+app.include_router(albums.router)
+app.include_router(songs.router)
 
 @app.get("/")
 async def root():
